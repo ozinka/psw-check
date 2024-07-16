b0d2fb6d69d704da28086992e8e1c93c3b2bf729
Ozi Os <tech.emu@gmail.com>
2024-07-16 20:47:10 +0300

    small updates in logs output, index.html

--- flask_main.py
+++ flask_main.py
@@ -1,3 +1,17 @@
+"""
+File Name: search.py
+Description: Flask application allows to go through the big (hundreds of GB) TXT file with sorted lines and
+             does search for specific string (password)
+
+Created by: Vitaliy Osidach
+Date Created: 2024.07.16
+
+Version: 1.0
+
+Usage:
+    run
+    python3 flask_main.py
+"""
 from flask import Flask, render_template, request
 import search
 
--- search.py
+++ search.py
@@ -1,3 +1,16 @@
+"""
+File Name: search.py
+Description: Module contains one function to go through the big (hundreds of GB) TXT file with sorted lines and
+             does search for specific string (password)
+
+Created by: Vitaliy Osidach
+Date Created: 2024.07.16
+
+Version: 1.0
+
+Usage:
+    search('some_text')
+"""
 import time
 
 
@@ -5,7 +18,7 @@ def search(st: str) -> tuple:
     start_time = time.time()
     res = []
     res.append([])
-    with open(r"c:\Users\kylik\Downloads\rockyou2024\rockyou2024.txt", mode='r', encoding="utf-8",
+    with open("rockyou2024.txt", mode='r', encoding="utf-8",
               errors='ignore') as f:
         start = 0
         f.seek(0, 2)  # os.SEEK_END = 2
@@ -15,40 +28,36 @@ def search(st: str) -> tuple:
         f.seek(pos)
         f.readline()
         line = f.readline().strip()
-        res[0].append(f'Pos {i}:{pos * 100 // max}%, line: {line}')
+        res[0].append(f'Pos {i}: {pos * 100 / max:.2f}%, line: {line:.10}')
         while (end - start) > 300:
             if st == line:
                 start, end = pos - 100, pos + 100
                 res[0].append(f'password is found')
                 break
             elif st > line:
-                start, pos, end = pos - 100, (pos + end) // 2, end
+                start, pos, end = pos - 100, (pos + end) // 2, end # 100 - edge effect, should be optimized
                 f.seek(pos)
                 f.readline()
                 line = f.readline().strip()
-                res[0].append(f'Pos {i}:{pos * 100 // max}%, line: {line}')
+                res[0].append(f'Pos {i}: {pos * 100 // max}%, line: {line:.10}')
             else:
-                start, pos, end = start - 100, (pos + start) // 2, pos + 100
+                start, pos, end = start - 100, (pos + start) // 2, pos + 100# 100 - edge effect, should be optimized
                 f.seek(pos)
                 f.readline()
                 line = f.readline().strip()
-                res[0].append(f'Pos {i}:{pos * 100 // max}%, line: {line}')
-                i += 1
+                res[0].append(f'Pos {i}: {pos * 100 // max}%, line: {line:.10}')
+            i += 1
         f.seek(start)
         f.readline()
         res.append([])
         while start < end - 20:
             res[1].append(f.readline().strip())
             start = f.tell()
-        t = str((time.time() - start_time) * 1000)
-        print(t)
-        t = t[:t.find('.')] + t[t.find('.'):t.find('.') + 4] # 234234.234234
-        print(t)
-    return res[0], res[1], f'Search time: {int((time.time() - start_time) * 1000)} ms'
+    return res[0], res[1], f'Search time: {(time.time() - start_time) * 1000:.2f} ms'
 
 
 if __name__ == '__main__':
-    print(search('l0pihara'))
+    print(search('asdf'))
 
 # START -------*-------- POS ------------- END
 # START ------POS------- END -------------
